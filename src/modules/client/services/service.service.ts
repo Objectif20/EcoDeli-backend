23f769f94e7ac1756f690b7de075030429cc6eab
Rémy <remy.thibaut2005@gmail.com>
2025-04-20 19:09:34 +0200

    Endpoint du détails d'une prestation

--- src/modules/client/services/service.service.ts
+++ src/modules/client/services/service.service.ts
@@ -255,13 +255,95 @@ export class ServiceService {
 }
 
   async getServiceDetails(service_id: string) {
-    const service = await this.serviceRepo.findOne({
+    const serviceList = await this.serviceRepo.findOne({
       where: { service_id },
-      relations: ['images'],
+      relations: [
+        'services',
+        'services.provider',
+        'services.provider.user',
+        'images',
+        'keywords',
+        'keywords.keywordList',
+        'appointments',
+        'appointments.review_presta',
+        'appointments.review_presta.responses',
+        'appointments.client',
+        'appointments.client.user',
+      ],
     });
 
-    if (!service) throw new NotFoundException('Service introuvable');
-    return service;
+    if (!serviceList) {
+      throw new NotFoundException('Service introuvable');
+    }
+
+    const providerAppointments = serviceList.appointments;
+    const firstService = serviceList.services[0];
+
+    const imageUrls = await Promise.all(serviceList.images.map(image =>
+      this.minioService.generateImageUrl('provider-images', image.image_service_url)
+    ));
+
+    const authorPhotoUrl = firstService && firstService.provider && firstService.provider.user
+      ? await this.minioService.generateImageUrl('client-images', firstService.provider.user.profile_picture)
+      : null;
+
+    const comments = await Promise.all(providerAppointments.flatMap(async (appointment) => {
+      if (appointment.review_presta) {
+        const clientPhotoUrl = appointment.client.user
+          ? await this.minioService.generateImageUrl('client-images', appointment.client.user.profile_picture)
+          : null;
+
+        const responseAuthorPhotoUrl = firstService && firstService.provider && firstService.provider.user
+          ? await this.minioService.generateImageUrl('client-images', firstService.provider.user.profile_picture)
+          : null;
+
+        return [{
+          id: appointment.review_presta.review_presta_id,
+          author: {
+            id: appointment.client.client_id,
+            name: `${appointment.client.first_name} ${appointment.client.last_name}`,
+            photo: clientPhotoUrl,
+          },
+          content: appointment.review_presta.comment,
+          response: appointment.review_presta.responses?.length ? {
+            id: appointment.review_presta.responses[0].review_presta_response_id,
+            author: {
+              id: firstService.provider.provider_id,
+              name: `${firstService.provider.first_name} ${firstService.provider.last_name}`,
+              photo: responseAuthorPhotoUrl,
+            },
+            content: appointment.review_presta.responses[0].comment,
+          } : undefined,
+        }];
+      }
+      return [];
+    }));
+
+    const rate = providerAppointments.reduce((acc, appointment) => acc + (appointment.review_presta?.rating || 0), 0)
+      / (providerAppointments.filter(appointment => appointment.review_presta).length || 1);
+
+    return {
+      service_id: serviceList.service_id,
+      service_type: serviceList.service_type,
+      status: serviceList.status,
+      name: serviceList.name,
+      city: serviceList.city,
+      price: serviceList.price,
+      price_admin: serviceList.price_admin,
+      duration_time: serviceList.duration_minute,
+      available: serviceList.available,
+      keywords: serviceList.keywords.map(keyword => keyword.keywordList.keyword),
+      images: imageUrls,
+      description: serviceList.description,
+      author: firstService && firstService.provider && firstService.provider.user ? {
+        id: firstService.provider.provider_id,
+        name: `${firstService.provider.first_name} ${firstService.provider.last_name}`,
+        email: firstService.provider.user.email,
+        photo: authorPhotoUrl,
+      } : null,
+      rate,
+      comments,
+    };
   }
 
   async createAppointment(service_id: string, data: {user_id: string; service_date: Date }) {
