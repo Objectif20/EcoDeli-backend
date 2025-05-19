e8467a29e372b2f77e4652e42949836b2fd9f7b2
Rémy <remy.thibaut2005@gmail.com>
2025-05-19 23:33:37 +0200

    Ajout d'un module Shipment sur mobile + des endpoints pour mes demandes de livraison en cours + le détails de chacune

--- src/modules/mobile/mobile.module.ts
+++ src/modules/mobile/mobile.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientAuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { DeliveriesModule } from './deliveries/deliveries.module';
+import { ShipmentsModule } from './shipments/shipments.module';
 
 @Module({
-    imports: [ClientAuthModule, ProfileModule, DeliveriesModule],
+    imports: [ClientAuthModule, ProfileModule, DeliveriesModule, ShipmentsModule],
     providers: [],
 })
 export class MobileModule {}
