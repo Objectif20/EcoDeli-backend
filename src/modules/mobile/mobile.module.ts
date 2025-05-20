638ce6c4f7585129fee642eedd85e6a9e9b30629
Rémy <remy.thibaut2005@gmail.com>
2025-05-20 23:35:19 +0200

    Ajout du module Services pour les écrans sur mobile

--- src/modules/mobile/mobile.module.ts
+++ src/modules/mobile/mobile.module.ts
@@ -3,9 +3,10 @@ import { ClientAuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { DeliveriesModule } from './deliveries/deliveries.module';
 import { ShipmentsModule } from './shipments/shipments.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-    imports: [ClientAuthModule, ProfileModule, DeliveriesModule, ShipmentsModule],
+    imports: [ClientAuthModule, ProfileModule, DeliveriesModule, ShipmentsModule,ServicesModule],
     providers: [],
 })
 export class MobileModule {}
